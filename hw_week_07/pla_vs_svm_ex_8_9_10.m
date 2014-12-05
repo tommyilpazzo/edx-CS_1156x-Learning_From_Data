401efd0636854066f3d8ff648724351fedd8736b
Tommaso Bolis <tommyilpazzo@gmail.com>
2014-12-05 13:00:42 +0100

    updated week 7 homework

--- hw_week_07/pla_vs_svm_ex_8_9_10.m
+++ hw_week_07/pla_vs_svm_ex_8_9_10.m
@@ -16,7 +16,7 @@ C = Inf;
 runs = 1000;
 
 % Linear kernel
-k = @ (x, xp) x * xp';
+kernel_function = @ (x, xp) x * xp';
 
 for r = 1:runs
    
@@ -89,49 +89,39 @@ for r = 1:runs
     N = length (X);
     
     % Linear coefficient
-    q = ones (N, 1) .* -1;
+    f = -ones(N, 1);
     
     % Subject to
-    A = y';
-    b = 0;
+    Aeq = y';
+    beq = 0;
     
     % Lower and upper bounds
     lb = zeros (N, 1);
     ub = ones (N, 1) * C;
     
     % Other options
-    options = optimset('MaxIter', maxIters);
+    options = optimoptions('quadprog','Algorithm','interior-point-convex','Display','off','MaxIter', maxIters);
     
     % Quadratic coefficients
     H = [];
 
     for i = 1:N
         for j = 1:N
-            H(i,j) = y(i) * y(j) * k (X(i,:), X(j,:)); %#ok<*SAGROW>
+            H(i,j) = y(i) * y(j) * kernel_function (X(i,:), X(j,:)); %#ok<*SAGROW>
         end
     end
-    
-    % Initial guess for the alphas
-    x0 = [];
-    
-    % Adds some noise to make things easier for qp ()
-    Hm = H + eye(N) .* 1e-15;
-    
-    % Uses the slightly modified matrix to get a reliable initial guess for the alphas
-    % Without this step, qp () fails to converge
-    %x0 = quadprog(Hm, q, A, b, [], [], lb, [], x0, options);
-    
+            
     % Solves the quadratic programming to get the final alphas
-    a = quadprog(H, q, A, b, [], [], lb, [], [], options);
+    alpha = quadprog(H, f, [], [], Aeq, beq, lb, [], [], options);
     
     % Finds the support vectors
-    sv = find(a > 1e-7);
+    sv = find(alpha > 1e-7);
 
     % Returns weight vector w from x
-    w = [sum(a(sv) .* y(sv) .* X(sv, 1)); sum(a(sv) .* y(sv) .* X(sv, 2))];
+    w = [sum(alpha(sv) .* y(sv) .* X(sv, 1)); sum(alpha(sv) .* y(sv) .* X(sv, 2))];
 
     % Returns the b value using the first support vector found
-    b = y(sv(1)) - sum (a(sv) .* y(sv) .* k (X(sv, :), X(sv(1), :)));
+    b = y(sv(1)) - sum (alpha(sv) .* y(sv) .* kernel_function (X(sv, :), X(sv(1), :)));
 
     % B is the x0
     w = [b; w]; %#ok<AGROW>
